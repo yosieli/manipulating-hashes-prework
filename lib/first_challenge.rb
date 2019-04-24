09dcde5641c1a64fc7dc5de1d7f00ca5138dd076
feven <feventsegay6@gmail.com>
2019-04-24 16:27:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 contacts.each do |person,data|
-data.each do|attribute,valve| 
+data.each do|attribute,value| 
 if attribute==:favorite_icecream_flavors
         value.delete_if {|flavor| flavor=="strawberry"}
 #remember to return your newly altered contacts hash!
